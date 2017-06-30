8c6b614a24da9f814cb52e767f2e705b3d5e9f89
Tong Sun <suntong@cpan.org>
2017-06-29 21:00:44 -0400

    - [+] example_test.go add one more test sample

--- example_test.go
+++ example_test.go
@@ -12,23 +12,27 @@ func Example_output() {
 	var docs = [][]byte{
 		[]byte("this is a test phrase"),
 		[]byte("this is a test phrass"),
+		[]byte("these are test phrases"),
 		[]byte("foo bar"),
 	}
 
 	hashes := make([]uint64, len(docs))
 	for i, d := range docs {
 		hashes[i] = simhash.Simhash(simhash.NewWordFeatureSet(d))
-		fmt.Printf("Simhash of %s: %x\n", d, hashes[i])
+		fmt.Printf("Simhash of '%s': %x\n", d, hashes[i])
 	}
 
 	fmt.Printf("Comparison of `%s` and `%s`: %d\n", docs[0], docs[1], simhash.Compare(hashes[0], hashes[1]))
 	fmt.Printf("Comparison of `%s` and `%s`: %d\n", docs[0], docs[2], simhash.Compare(hashes[0], hashes[2]))
+	fmt.Printf("Comparison of `%s` and `%s`: %d\n", docs[0], docs[3], simhash.Compare(hashes[0], hashes[3]))
 
 	// Output:
-	// Simhash of this is a test phrase: 8c3a5f7e9ecb3f35
-	// Simhash of this is a test phrass: 8c3a5f7e9ecb3f21
-	// Simhash of foo bar: d8dbe7186bad3db3
+	// Simhash of 'this is a test phrase': 8c3a5f7e9ecb3f35
+	// Simhash of 'this is a test phrass': 8c3a5f7e9ecb3f21
+	// Simhash of 'these are test phrases': ddfdbf7fbfaffb1d
+	// Simhash of 'foo bar': d8dbe7186bad3db3
 	// Comparison of `this is a test phrase` and `this is a test phrass`: 2
+	// Comparison of `this is a test phrase` and `these are test phrases`: 22
 	// Comparison of `this is a test phrase` and `foo bar`: 29
 
 }
