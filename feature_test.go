e82bb3e75c96719859bd7013ed53d9bb5c27cea3
Tong Sun <suntong@cpan.org>
2017-06-30 22:40:14 -0400

    - [+] add ExampleNewWordFeatureSet & ExampleNewUnicodeWordFeatureSet tests

--- feature_test.go
+++ feature_test.go
@@ -5,8 +5,10 @@
 package simhash
 
 import (
-	"golang.org/x/text/unicode/norm"
+	"fmt"
 	"testing"
+
+	"golang.org/x/text/unicode/norm"
 )
 
 func TestNewFeature(t *testing.T) {
@@ -57,6 +59,17 @@ func TestFeatureSet(t *testing.T) {
 	}
 }
 
+func ExampleNewWordFeatureSet() {
+	text := []byte("a a abc abc test test string.")
+	fs := NewWordFeatureSet(text)
+	fmt.Printf("%#v\n", fs)
+	actual := fs.GetFeatures()
+	fmt.Printf("%#v\n", actual)
+	// Output:
+	// &simhash.WordFeatureSet{b:[]uint8{0x61, 0x20, 0x61, 0x20, 0x61, 0x62, 0x63, 0x20, 0x61, 0x62, 0x63, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x2e}}
+	// []simhash.Feature{simhash.feature{sum:0xaf63bd4c8601b7be, weight:1}, simhash.feature{sum:0xaf63bd4c8601b7be, weight:1}, simhash.feature{sum:0xd8dcca186bafadcb, weight:1}, simhash.feature{sum:0xd8dcca186bafadcb, weight:1}, simhash.feature{sum:0x8c093f7e9fccbf69, weight:1}, simhash.feature{sum:0x8c093f7e9fccbf69, weight:1}, simhash.feature{sum:0x9926dcde0a17d48e, weight:1}}
+}
+
 func TestUnicodeWordFeatureSet(t *testing.T) {
 	text := []byte("la fin d'un bel après-midi d'été")
 	fs := NewUnicodeWordFeatureSet(text, norm.NFKC)
@@ -79,6 +92,28 @@ func TestUnicodeWordFeatureSet(t *testing.T) {
 			t.Errorf("feature.Weight(): expected %d, actual %d", expected[i].Weight(), actual[i].Weight())
 		}
 	}
+}
+
+func ExampleNewUnicodeWordFeatureSet_inWestern() {
+	text := []byte("la fin d'un bel après-midi d'été")
+	fs := NewUnicodeWordFeatureSet(text, norm.NFKC)
+	fmt.Printf("%#v\n", fs)
+	actual := fs.GetFeatures()
+	fmt.Printf("%#v\n", actual)
+	// Output:
+	// &simhash.UnicodeWordFeatureSet{b:[]uint8{0x6c, 0x61, 0x20, 0x66, 0x69, 0x6e, 0x20, 0x64, 0x27, 0x75, 0x6e, 0x20, 0x62, 0x65, 0x6c, 0x20, 0x61, 0x70, 0x72, 0xc3, 0xa8, 0x73, 0x2d, 0x6d, 0x69, 0x64, 0x69, 0x20, 0x64, 0x27, 0xc3, 0xa9, 0x74, 0xc3, 0xa9}, f:2}
+	// []simhash.Feature{simhash.feature{sum:0x8325c07b4eb2548, weight:1}, simhash.feature{sum:0xd8cbc5186ba13198, weight:1}, simhash.feature{sum:0x15cdbd7eed98cfab, weight:1}, simhash.feature{sum:0xd8d9a1186bad324a, weight:1}, simhash.feature{sum:0x3adb901f8c8a7b5e, weight:1}, simhash.feature{sum:0x7e8f29c36ffb774e, weight:1}}
+}
+
+func ExampleNewUnicodeWordFeatureSet_inChinese() {
+	text := []byte("当山峰没有棱角的时候")
+	fs := NewUnicodeWordFeatureSet(text, norm.NFKC)
+	fmt.Printf("%#v\n", fs)
+	actual := fs.GetFeatures()
+	fmt.Printf("%#v\n", actual)
+	// Output:
+	// &simhash.UnicodeWordFeatureSet{b:[]uint8{0xe5, 0xbd, 0x93, 0xe5, 0xb1, 0xb1, 0xe5, 0xb3, 0xb0, 0xe6, 0xb2, 0xa1, 0xe6, 0x9c, 0x89, 0xe6, 0xa3, 0xb1, 0xe8, 0xa7, 0x92, 0xe7, 0x9a, 0x84, 0xe6, 0x97, 0xb6, 0xe5, 0x80, 0x99}, f:2}
+	// []simhash.Feature{simhash.feature{sum:0xa5edea16c0c7a180, weight:1}}
 }
 
 func TestGetFeatures(t *testing.T) {
